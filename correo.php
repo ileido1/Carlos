252eda31afb87a700a8d974a02e90f5946abc12e
Carlos H <carlosluishc@hotmail.com>
2018-05-27 12:19:59 -0400

    1.3

---
+++ correo.php
@@ -0,0 +1,20 @@
+<?php
+		$nombre= $_POST["nombre"];
+		$direccion= $_POST["direccion"];
+		$coreo= $_POST["email"];
+		$telefono= $_POST["telefono"];
+		$asunto= $_POST["asunto"];
+		$requirimiento= $_POST["requirimiento"];
+
+				$texto = "Nueva solicitud.\n";
+				$texto.= $nombre .  $correo .  $telefono;
+				$texto. = "\n";
+				$texto = $asunto . $requirimiento;
+				$destino ="soycarlosluis862@gmail.com";
+				$asunto="Notificacion";
+				$header="MIME-Version:1.\r\n";
+				$header.="Content-type: text/html; charset= UTF-8\r\n";
+				$header.="From: cl < cl@hernandez.com >\r\n";
+				$exito=mail($destino, $asunto, $texto,$header);
+				echo "1";	
+  ?>
--- validation.js
+++ validation.js
@@ -31,6 +31,10 @@ $("#form").validate({
 				required: 'No puede dejar este campo en blanco',
 
 			},
+			email:{
+				required: 'No puede dejar este campo en blanco',
+
+			},
 			direccion:{
 				required: 'No puede dejar este campo en blanco'
 			},
@@ -45,6 +49,38 @@ $("#form").validate({
 				required: 'No puede dejar este campo en blanco'
 			},
 		}
+});
+ $("#form").on("submit", function() {
+	var esvalido = $('#form').valid();
+	if(esvalido){
+	var data = $('#form').serialize();
+	$.ajax({
+		url: 'correo.php',
+		type: 'POST',
+		dataType: 'json',
+		data: data,
+		beforeSend: function(){
+			$('.spin').css('display','inline');
+		},
+		success:function(r){
+			if (r==1) {
+				var alertBox = '<div class="alert alert-success alert-dismissable" style="margin-top:20px;"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a> Tu informacion se ha enviado correctamente</div>'
+                 $('#form').find('.messages').html(alertBox);
+			}
+			else{
+                var alertBox = '<div class="alert alert-danger alert-dismissable"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>Verifique el captcha</div>'
+                $('#msform').find('.messages').html(alertBox);
+                var dest = $("#msform").offset().top;
+				$("html, body").animate({scrollTop: dest});
+		}
+		setTimeout(function(){
+		$('.spin').hide();
+		}, 1000);
+	}
+
+	});
+	return false;
+}
 });
 
 });
